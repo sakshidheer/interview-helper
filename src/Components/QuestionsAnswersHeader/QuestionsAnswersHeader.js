3dc7b5ac96112b082999ac274a41c7968442d32f
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-03-24 23:54:35 +0530

    Adding add, grid and list button

--- src/Components/QuestionsAnswersHeader/QuestionsAnswersHeader.js
+++ src/Components/QuestionsAnswersHeader/QuestionsAnswersHeader.js
@@ -1,8 +1,26 @@
+import { Button, IconButton } from '@material-ui/core';
 import { IoMdAdd } from 'react-icons/io';
+import { BsFillGrid3X3GapFill, BsList } from 'react-icons/bs';
 import classes from './QuestionsAnswersHeader.module.css'
-const QuestionsAnswersHeader = () =>{
+const QuestionsAnswersHeader = () => {
+    
     return <div className={classes.header}>
-        <IoMdAdd size="1.5em" color='#f50057'/>
+        <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            startIcon={<IoMdAdd />}
+        >
+            Add
+        </Button>
+
+        <IconButton aria-label="grid" edge="end" color="secondary">
+            <BsFillGrid3X3GapFill/>
+        </IconButton>
+        <IconButton aria-label="list" color="primary">
+            <BsList/>
+        </IconButton>
+
     </div>
 }
 
