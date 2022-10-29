a522533d8bf7375aae1b9e03d703d291730ae154
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-10-29 18:03:21 +0530

    Adding experince

--- src/Components/QuestionsAnswersHeader/Filter/Filter.js
+++ src/Components/QuestionsAnswersHeader/Filter/Filter.js
@@ -7,11 +7,14 @@ import classes from './Filter.module.css';
 
 const Filter = () => {
     let [lang,setLang] =useState('');
+    let [experience, setExperience] = useState('');
+    let [text,setText] = useState('');
     return (<div className={classes.filter}>
         <FormControl style={{ maxWidth: 150 }} variant="standard">
             <TextField
                 id="text"
                 label="Text"
+                value={text}
                 variant="outlined"
                 InputLabelProps={{
                     shrink: true,
@@ -19,7 +22,7 @@ const Filter = () => {
             />
         </FormControl>
         <Languages lang={lang}/>
-        <Experience />
+        <Experience experience={experience}/>
         <Button color="secondary" variant="contained">
             Search
         </Button>
